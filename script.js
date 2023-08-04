5c80459205e49a8c428ff8fe30183f7924fe5a7f
https://pokemon-infinite-fusion.coderobo.org/ <141412731+game-website@users.noreply.github.com>
2023-08-04 16:56:39 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+const aegislashStats = [
+    {"base_stat": 60},
+    {"base_stat": 50},
+    {"base_stat": 150},
+    {"base_stat": 50},
+    {"base_stat": 150},
+    {"base_stat": 60}
+]
+
+//Butterfree
+const butterfreeStats = [
+    {"base_stat": 60},
+    {"base_stat": 45},
+    {"base_stat": 50},
+    {"base_stat": 80},
+    {"base_stat": 80},
+    {"base_stat": 70}
+]
+
+//Beedrill
+const beedrillStats = [
+    {"base_stat": 65},
+    {"base_stat": 80},
+    {"base_stat": 40},
+    {"base_stat": 45},
+    {"base_stat": 80},
+    {"base_stat": 75}
+]
